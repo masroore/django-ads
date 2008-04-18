dcbcc8564915c8864c84044c7495e1d3f773f299
marinho <marinho@263815be-c448-0410-9ef5-69a194ea20f0>
2008-04-18 22:43:19 +0000

    new changes for advertising system

--- trunk/ads/media/ads_client.js
+++ trunk/ads/media/ads_client.js
@@ -6,8 +6,41 @@
 if (cont_id != undefined && website_id != undefined && adbox_id != undefined) {
     var ADS_SERVER_URL = 'http://localhost:8000/';
     var ADS_SERVER_GET_ADS_URL = ADS_SERVER_URL+'ads/website/'+website_id+'/adbox/'+adbox_id+'/get_ads/';
+    var MAX_WORDS = 100;
+    var INVALID_WORDS = ['this','the']
+    var collected_words = [];
 
-    $('#'+cont_id).before('<iframe id="'+cont_id+'_f" src="'+ADS_SERVER_GET_ADS_URL+'"/>');
+    letters_count = function(txt) {
+        var ret = 0;
+        
+        for (var i=0; i<txt.length; i++) {
+            var c = txt.charCodeAt(i);
+            ret += (c >= 65 && c <= 90) || (c >= 97 && c <= 122) ? 1 : 0 ;
+        }
+
+        return ret;
+    }
+
+    get_words = function(txt){
+        var words = txt.split(' ');
+
+        for (var i=0; i<words.length; i++) {
+            w = words[i].toLowerCase();
+
+            if (letters_count(w) >= 3 && collected_words.indexOf(w) == -1 && INVALID_WORDS.indexOf(w) == -1) {
+                collected_words.push(w);
+            }
+        }
+    }
+
+    // Parses document for relevant words
+    $('title, h1, h2, h3, h4').each(function(){ get_words($(this).text()); });
+    $('meta[name=keywords], meta[name=description]').each(function(){ get_words(this.content); });
+
+    var collected_words = collected_words.length <= MAX_WORDS ? collected_words : collected_words.slice(0,MAX_WORDS);
+
+    // Generate iframe with its properties
+    $('#'+cont_id).before('<iframe id="'+cont_id+'_f" src="'+ADS_SERVER_GET_ADS_URL+'?words='+collected_words+'"/>');
     $('#'+cont_id).remove();
 
     $('#'+cont_id+'_f').css('height', adbox_height+'px');
