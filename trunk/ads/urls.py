fff21c223f49a3808f09629600cf4f67d1c673b2
marinho <marinho@263815be-c448-0410-9ef5-69a194ea20f0>
2008-04-21 12:15:22 +0000

    fixing CRUD features

--- trunk/ads/urls.py
+++ trunk/ads/urls.py
@@ -5,15 +5,21 @@ urlpatterns = patterns('apps.ads.views',
     (r'^create_account/$', 'create_account'),
     (r'^create_advertiser/$', 'advertiser_create'),
     (r'^create_website/$', 'website_create'),
+    (r'^login/$', 'login'),
+    (r'^logout/$', 'logout'),
 
     (r'^advertiser/(?P<advertiser_id>\d+)/$', 'advertiser_home'),
-    (r'^advertiser/(?P<advertiser_id>\d+)/create_ad/$', 'ad_create'),
+    (r'^advertiser/(?P<advertiser_id>\d+)/create_ad/$', 'ad_edit'),
     (r'^advertiser/(?P<advertiser_id>\d+)/ad/(?P<ad_id>\d+)/$', 'ad_home'),
+    (r'^advertiser/(?P<advertiser_id>\d+)/ad/(?P<ad_id>\d+)/edit/$', 'ad_edit'),
+    (r'^advertiser/(?P<advertiser_id>\d+)/ad/(?P<ad_id>\d+)/delete/$', 'ad_delete'),
     (r'^advertiser/(?P<advertiser_id>\d+)/ad/(?P<ad_id>\d+)/hit/$', 'ad_hit'),
     
     (r'^website/(?P<website_id>\d+)/$', 'website_home'),
-    (r'^website/(?P<website_id>\d+)/create_adbox/$', 'adbox_create'),
+    (r'^website/(?P<website_id>\d+)/create_adbox/$', 'adbox_edit'),
     (r'^website/(?P<website_id>\d+)/adbox/(?P<adbox_id>\d+)/$', 'adbox_home'),
+    (r'^website/(?P<website_id>\d+)/adbox/(?P<adbox_id>\d+)/edit/$', 'adbox_edit'),
+    (r'^website/(?P<website_id>\d+)/adbox/(?P<adbox_id>\d+)/delete/$', 'adbox_delete'),
     (r'^website/(?P<website_id>\d+)/adbox/(?P<adbox_id>\d+)/get_ads/$', 'adbox_get_ads'),
 )
 
--- trunk/ads/views.py
+++ trunk/ads/views.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import ugettext as _
@@ -11,13 +11,12 @@ from django.contrib.djangoplus.shortcuts import render_to_json
 from django.conf import settings
 from django.template.defaultfilters import slugify
 from django.views.decorators.cache import never_cache
+from django.contrib.auth.forms import UserCreationForm
 
-from forms import FormAccount, FormAdvertiser, FormWebsite, FormAd, FormAdBox
+from forms import FormAdvertiser, FormWebsite, FormAd, FormAdBox, FormLogin
 from models import Website, Advertiser, Ad, AdBox
 
 def index(request):
-    form_account = FormAccount()
-
     return render_to_response(
             'ads/index.html',
             locals(),
@@ -26,25 +25,49 @@ def index(request):
 
 def create_account(request):
     if request.POST:
-        form_account = FormAccount(request.POST)
+        form_account = UserCreationForm(request.POST)
 
         if form_account.is_valid():
             user = form_account.save()
             if user:
                 user = authenticate(
-                        username=user.username,
-                        password=form_account.cleaned_data['password']
+                        username=form_account.cleaned_data['username'],
+                        password=form_account.cleaned_data['password1']
                         )
-                login(request, user)
+                auth_login(request, user)
                 return HttpResponseRedirect('/ads/')
     else:
-        form_account = FormAccount()
+        form_account = UserCreationForm()
 
     return render_to_response(
             'ads/create_account.html',
             locals(),
             context_instance=RequestContext(request),
             )
+
+def login(request):
+    if request.POST:
+        form_login = FormLogin(request.POST)
+
+        if form_login.is_valid():
+            user = authenticate(
+                        username=form_login.cleaned_data['username'],
+                        password=form_login.cleaned_data['password']
+                        )
+            auth_login(request, user)
+            return HttpResponseRedirect('/ads/')
+    else:
+        form_login = FormLogin()
+
+    return render_to_response(
+            'ads/login.html',
+            locals(),
+            context_instance=RequestContext(request),
+            )
+
+def logout(request):
+    auth_logout(request)
+    return HttpResponseRedirect('/ads/')
 
 @login_required
 def advertiser_create(request):
@@ -105,25 +128,36 @@ def website_home(request, website_id):
             )
 
 @login_required
-def ad_create(request, advertiser_id):
+def ad_edit(request, advertiser_id, ad_id=None):
     advertiser = get_object_or_404(Advertiser, id=advertiser_id)
+    ad = ad_id and get_object_or_404(Ad, id=ad_id) or None
 
     if request.POST:
-        form_ad = FormAd(request.POST)
+        form_ad = FormAd(request.POST, instance=ad)
 
         if form_ad.is_valid():
             ad = form_ad.save(advertiser)
             if ad:
-                request.user.message_set.create(message=_('Ad created!'))
+                request.user.message_set.create(message=_('Ad saved!'))
                 return HttpResponseRedirect(advertiser.get_absolute_url())
     else:
-        form_ad = FormAd()
+        form_ad = FormAd(instance=ad)
 
     return render_to_response(
             'ads/ad_edit.html',
             locals(),
             context_instance=RequestContext(request),
             )
+
+@login_required
+def ad_delete(request, advertiser_id, ad_id):
+    advertiser = get_object_or_404(Advertiser, id=advertiser_id)
+    ad = get_object_or_404(Ad, id=ad_id)
+
+    ad.delete()
+
+    request.user.message_set.create(message=_('Ad Deleted!'))
+    return HttpResponseRedirect('/ads/')
 
 def ad_home(request, advertiser_id, ad_id):
     advertiser = get_object_or_404(Advertiser, id=advertiser_id)
@@ -147,11 +181,12 @@ def ad_hit(request, advertiser_id, ad_id):
         return HttpResponseRedirect(settings.PROJECT_ROOT_URL)
 
 @login_required
-def adbox_create(request, website_id):
+def adbox_edit(request, website_id, adbox_id=None):
     website = get_object_or_404(Website, id=website_id)
+    adbox = adbox_id and get_object_or_404(AdBox, id=adbox_id) or None
 
     if request.POST:
-        form_adbox = FormAdBox(request.POST)
+        form_adbox = FormAdBox(request.POST, instance=adbox)
 
         if form_adbox.is_valid():
             adbox = form_adbox.save(False)
@@ -159,16 +194,26 @@ def adbox_create(request, website_id):
             if adbox:
                 adbox.website = website
                 adbox.save()
-                request.user.message_set.create(message=_('AdBox created!'))
+                request.user.message_set.create(message=_('AdBox saved!'))
                 return HttpResponseRedirect(website.get_absolute_url())
     else:
-        form_adbox = FormAdBox()
+        form_adbox = FormAdBox(instance=adbox)
 
     return render_to_response(
             'ads/adbox_edit.html',
             locals(),
             context_instance=RequestContext(request),
             )
+
+@login_required
+def adbox_delete(request, website_id, adbox_id):
+    website = get_object_or_404(Website, id=website_id)
+    adbox = get_object_or_404(AdBox, id=adbox_id)
+
+    adbox.delete()
+
+    request.user.message_set.create(message=_('AdBox Deleted!'))
+    return HttpResponseRedirect('/ads/')
 
 def adbox_home(request, website_id, adbox_id):
     website = get_object_or_404(Website, id=website_id)
