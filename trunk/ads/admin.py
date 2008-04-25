e768798688ea0cbf41a06a0745697bda7741a2d4
marinho <marinho@263815be-c448-0410-9ef5-69a194ea20f0>
2008-04-25 12:54:35 +0000

    fixin admin

--- trunk/ads/admin.py
+++ trunk/ads/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib.admin.options import ModelAdmin
 
 from models import Show, Advertiser, AdvertiserUser, Website, WebsiteUser, \
-        AdModel, AdBox, Word, URL, Ad, Log, StyleSet
+        AdModel, AdBox, Word, URL, Ad, Log, StyleSet, CreditsLog
 
 class ShowAdmin(ModelAdmin):
     list_display = ('group','name','slug','url_pattern',)
@@ -13,13 +13,13 @@ class AdvertiserAdmin(ModelAdmin):
     pass
 
 class AdvertiserUserAdmin(ModelAdmin):
-    pass
+    raw_id_fields = ('user','advertiser',)
 
 class WebsiteAdmin(ModelAdmin):
     pass
 
 class WebsiteUserAdmin(ModelAdmin):
-    pass
+    raw_id_fields = ('user','website',)
 
 class AdModelAdmin(ModelAdmin):
     pass
@@ -28,23 +28,27 @@ class AdModelAdmin(ModelAdmin):
     pass
 
 class AdBoxAdmin(ModelAdmin):
-    pass
+    raw_id_fields = ('website',)
 
 class WordAdmin(ModelAdmin):
     pass
 
 class URLAdmin(ModelAdmin):
-    pass
+    raw_id_fields = ('website',)
 
 class AdAdmin(ModelAdmin):
     list_display = ('title','advertiser','view_credits','click_credits','view_count','click_count','enabled','all_words','limited_by_credits')
     list_filter = ('enabled','all_words','limited_by_credits')
+    raw_id_fields = ('advertiser','words',)
 
 class LogAdmin(ModelAdmin):
-    pass
+    raw_id_fields = ('ad','website_url',)
 
 class StyleSetAdmin(ModelAdmin):
     pass
+
+class CreditsLogAdmin(ModelAdmin):
+    raw_id_fields = ('ad',)
 
 admin.site.register(Show, ShowAdmin)
 admin.site.register(Advertiser, AdvertiserAdmin)
@@ -58,4 +62,5 @@ admin.site.register(URL, URLAdmin)
 admin.site.register(Ad, AdAdmin)
 admin.site.register(Log, LogAdmin)
 admin.site.register(StyleSet, StyleSetAdmin)
+admin.site.register(CreditsLog, CreditsLogAdmin)
 
