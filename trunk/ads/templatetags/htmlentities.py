b1b03d217652d129e1f8032dbe0c60456228027f
marinho <marinho@263815be-c448-0410-9ef5-69a194ea20f0>
2008-04-25 11:40:54 +0000

    htmlentities

---
+++ trunk/ads/templatetags/htmlentities.py
@@ -0,0 +1,14 @@
+# By pytechd
+# http://www.djangosnippets.org/snippets/556/
+
+from django import template
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+
+
+register = template.Library()
+
+
+@register.filter()
+def htmlentities(s):
+    return mark_safe(escape(s).encode('ascii', 'xmlcharrefreplace'))
