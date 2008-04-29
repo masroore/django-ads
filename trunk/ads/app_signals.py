d8b7f970d2c809e7da589528d65136988cfc55aa
marinho <marinho@263815be-c448-0410-9ef5-69a194ea20f0>
2008-04-29 19:28:52 +0000

    new signal get_meta_info to get informations about location and others

---
+++ trunk/ads/app_signals.py
@@ -0,0 +1,16 @@
+"""
+How to use it:
+
+# SIGNALS AND LISTENERS
+from django.dispatch import dispatcher
+from apps.ads import app_signals as ads_signals
+
+def get_meta_info(sender, meta):
+    meta.update({'COUNTRY_CODE':'BR', 'GMT_DIFF':-3})
+
+dispatcher.connect(get_meta_info, signal=ads_signals.get_meta_info)
+
+"""
+
+get_meta_info = object()
+
